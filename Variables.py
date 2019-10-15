f1ae45f5904d78a8f8fcbcaf7e1f95f174e13f24
Linet17 <linet.nyamu2017@outlook.com>
2019-10-15 20:47:39 +0300

    data types final

--- Variables.py
+++ Variables.py
@@ -6,3 +6,31 @@ math = 75
 type_of_math = type(math)
 print (type_of_math)
 print (type(math))
+
+#float
+opening_balance = 100.0
+print (type(opening_balance))
+
+#boolean
+g= True
+h= False
+
+#string - any character/are literal
+name = "Linet Gacheri Nyamu"
+print(type(name))
+national_id = "30451178"
+print(type(national_id))
+#type casting - converting fom one data type to another,you only convert what makes sense
+national_id = int(national_id)
+print(type(national_id))
+
+sentence = "a small man called ng'ang'a can cast a large shadow"
+print(sentence)
+sentenceTwo = 'a small man called nganga can cast a large shadow'
+print(sentenceTwo)
+sentenceThree = """a small man called ng'ang'a,
+                who is a large man,
+                can cast a large shadow"""
+print(sentenceThree)
+
+
