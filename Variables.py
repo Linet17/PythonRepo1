9800720108a9a93784a11b9f68fe070980f18a8b
Linet17 <linet.nyamu2017@outlook.com>
2019-10-15 19:00:12 +0300

    data types1

---
+++ Variables.py
@@ -0,0 +1,8 @@
+# integer - a data type that holds whole numbers
+
+math = 75
+
+#type - a built-in function that shows a variables data type
+type_of_math = type(math)
+print (type_of_math)
+print (type(math))
