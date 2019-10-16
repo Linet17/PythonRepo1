53024a68d88ca0fcb9603be5cdb49f6f5d7a2341
Linet17 <linet.nyamu2017@outlook.com>
2019-10-16 18:42:36 +0300

    list1

---
+++ lists.py
@@ -0,0 +1,8 @@
+#lists - a collection of more than one variable,like an array.Python does not have arrays.Lists can hold anything including other lists.
+#lists are rep by []
+
+personal = []
+print = (type (personal))
+
+personal = ["Linet Nyamu",56,"Nairobi",True,458852.00]
+print(len(personal))
