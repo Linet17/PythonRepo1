45c3b0705972277ee77599cf1d0d930a06fab2aa
Linet17 <linet.nyamu2017@outlook.com>
2019-10-17 11:12:17 +0300

    2nd commit

--- lists.py
+++ lists.py
@@ -1,21 +1,21 @@
-#lists - a collection of more than one variable,like an array.Python does not have arrays.Lists can hold anything including other lists.
-#lists are rep by []
+# lists - a collection of more than one variable,like an array.Python does not have arrays.Lists can hold anything including other lists.
+# lists are rep by []
 
 personal = []
-print = (type (personal))
+print = (type(personal))
 
 # personal = ["Linet Nyamu",56,"Nairobi",True,458852.00]
 # print(len(personal))
 
-students = ["Dennis","Linet","David"]
+students = ["Dennis", "Linet", "David"]
 print(students)
-#to print out Dennisli only
+# to print out Dennisli only
 print(students[0])
-#to slice Dennis out ie print dennis out as a list
+# to slice Dennis out ie print dennis out as a list
 print(students[0:1])
 
-#list operations
-#to join lists - concatenation
+# list operations
+# to join lists - concatenation
 newStudent = ["Jacky"]
 allStudents = students + newStudent
 print(allStudents)
@@ -25,24 +25,25 @@ allStudents.append(" ")
 print(allStudents)
 # allStudents.clear()
 
-#count
+# count
 allStudents.count("Geoffrey")
 
-#strip- removes spaces outside a string,is a string method
-#diff b2n method and function - methods belong to specific classes while functions are independent
-print(len("   Savio Muriithi  "))
-print(len("Savio Muriithi"))
+# strip- removes spaces outside a string,is a string method
+# diff b2n method and function - methods belong to specific classes while functions are independent
+# a = "   Savio Muriithi  "
+# print(a.index[0])
+# print(len("Savio Muriithi"))
 
-#KISS PRINCIPLE - Keep It Stup Simple
-#DRY PRINCIPLE - Dont Repeat Yourself
+# KISS PRINCIPLE - Keep It Stup Simple
+# DRY PRINCIPLE - Dont Repeat Yourself
 
 # position = allStudents.index("Jacky")
 # print(position)
 print(allStudents)
 print(allStudents[1:3])
 
-#insert - an element in a specific position
-allStudents.insert(0,"Kassim")
+# insert - an element in a specific position
+allStudents.insert(0, "Kassim")
 print(allStudents)
 allStudents.pop(1)
 print(allStudents)
